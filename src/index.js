19c017e0d6e005593ff7ac9808fa5e5ce5123fd6
yschristian <yubahwesc@gmail.com>
2022-01-22 12:43:13 -0800

    editted

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const Task = require('./models/task');
 const app = express();
 app.use(express.json())
 
+
+//creating
 app.post('/users',(req,res)=>{
      const user = new User(req.body)
         user.save().then(()=>{
@@ -22,6 +24,44 @@ app.post('/tasks',(req,res)=>{
      }).catch((e)=>{
          res.status(400).send(e);
      })
+})
+
+//reading
+app.get('/users',(req,res)=>{
+      User.find({}).then((users)=>{
+          res.send(users);
+      }).catch((e)=>{
+          res.status(500).send(e);
+        })
+})
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id;
+    User.findById(_id).then((user)=>{
+           if(!user){
+               res.status(404).send()
+           }
+           res.send(user);
+    }).catch((e)=>{
+         res.status(500).send()
+    })
+})
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        res.send(tasks);
+    }).catch((e)=>{
+       res.status(500).send(e);
+    })
+})
+app.get("/tasks/:id",(req,res)=>{
+    const _id = req.params.id;
+    Task.findById(_id).then((task)=>{
+          if(!task){
+              res.status(500).send();
+          }
+          res.status(201).send(task);
+    }).catch((e)=>{
+          res.status(500).send();
+    })
 })
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
