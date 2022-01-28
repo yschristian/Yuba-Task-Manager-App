d5f729435a5e146ba1398f15d179757400d844d5
ArseneMerci <harsenemerci@gmail.com>
2022-01-28 11:50:26 +0200

    fixed populate function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-require('./db/mongoose');
+const db =require('./db/mongoose');
 const userRouter  = require('./routers/user');
 const  taskerRouter = require('./routers/task');
 
+db()
 const app = express();
 app.use(express.json())
 app.use(userRouter)
@@ -16,10 +17,12 @@ app.listen(port,()=>{
 const Task  = require('./models/task')
 // const User  = require('./models/user')
   const  main = async  ()=>{
-  const  task  = await Task.findById('61f3988d8c6e312f6f78118c')
+  const  task  = await Task.findById('61f3988d8c6e312f6f78118c').populate('owner',['name','email','age']).catch((err)=>[
+    console.log(err.message)
+  ])
   //it  converts id into all profile of user
  //find user associate  with task
-  await task.populate('owner').execPopulate()
+
   console.log(task.owner)
 }
 main()
