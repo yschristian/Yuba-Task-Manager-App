688d119f675a0ffe5f725a85215a31751274f88b
yschristian <yubahwesc@gmail.com>
2022-01-30 12:18:58 -0800

    changed

--- src/routers/user.js
+++ src/routers/user.js
@@ -121,10 +121,21 @@ router.patch('/users/me',auth,async(req,res)=>{
 
 //upload profile pic
 const upload = multer({
-    dest:'avatars'
+    dest:'avatars',
+    limits:{
+       fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        if(!file.originalname.match('/\.(jpg|png|jpeg)$/')){
+         return cb(new Error('please upload an image') )
+        }
+        cb(undefined,true)
+    }
 })
 router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
     res.send()
+},(error,req,res,next)=>{
+    res.status(400).send({error: error.message})
 })
 
 module.exports = router
